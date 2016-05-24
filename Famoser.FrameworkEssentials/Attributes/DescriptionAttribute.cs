2d719fcdeba03d95d6266d73b151944d01c5cf91
famoser <git@famoser.ch>
2016-05-24 11:26:58 +0200

    added description attribute

---
+++ Famoser.FrameworkEssentials/Attributes/DescriptionAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Famoser.FrameworkEssentials.Attributes
+{
+    public class DescriptionAttribute : Attribute
+    {
+        public string Description { get; }
+        public DescriptionAttribute(string description)
+        {
+            Description = description;
+        }
+    }
+}
