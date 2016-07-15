645ee7f15be0b9154d481cb3415b1cfa2dcd8164
Florian Moser <git@famoser.ch>
2016-07-15 12:16:45 +0200

    added basehelper

---
+++ Famoser.FrameworkEssentials/Helpers/BaseHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Famoser.FrameworkEssentials.Logging;
+using Famoser.FrameworkEssentials.Logging.Interfaces;
+
+namespace Famoser.FrameworkEssentials.Helpers
+{
+    public class BaseHelper
+    {
+        private static IExceptionLogger _exceptionLogger = LogHelper.Instance;
+
+
+        protected static void SetExceptionLogger(IExceptionLogger exceptionLogger)
+        {
+            _exceptionLogger = exceptionLogger;
+        }
+
+        protected static T ExecuteSafe<T>(Func<T> func)
+        {
+            try
+            {
+                return func();
+            }
+            catch (Exception ex)
+            {
+                _exceptionLogger?.LogException(ex);
+            }
+            return default(T);
+        }
+
+        protected static async Task ExecuteSafe(Func<Task> func)
+        {
+            try
+            {
+                await func();
+            }
+            catch (Exception ex)
+            {
+                _exceptionLogger?.LogException(ex);
+            }
+            return;
+        }
+
+        protected static async Task<T> ExecuteSafe<T>(Func<Task<T>> func)
+        {
+            try
+            {
+                return await func();
+            }
+            catch (Exception ex)
+            {
+                _exceptionLogger?.LogException(ex);
+            }
+            return default(T);
+        }
+    }
+}
--- Famoser.FrameworkEssentials/Services/Base/BaseService.cs
+++ Famoser.FrameworkEssentials/Services/Base/BaseService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Famoser.FrameworkEssentials.Logging.Interfaces;
@@ -43,6 +44,40 @@ namespace Famoser.FrameworkEssentials.Services.Base
                 return default(T);
             }
             return func();
+        }
+
+        protected async Task Execute(Func<Task> func)
+        {
+            if (_catchExceptions)
+            {
+                try
+                {
+                    await func();
+                }
+                catch (Exception ex)
+                {
+                    _logger?.LogException(ex, this);
+                }
+                return;
+            }
+            await func();
+        }
+
+        protected async Task<T> Execute<T>(Func<Task<T>> func)
+        {
+            if (_catchExceptions)
+            {
+                try
+                {
+                    return await func();
+                }
+                catch (Exception ex)
+                {
+                    _logger?.LogException(ex, this);
+                }
+                return default(T);
+            }
+            return await func();
         }
     }
 }
